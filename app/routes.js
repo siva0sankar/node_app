20d285e3182b1af93859f19ed711ad541a0b9778
Sankar Siva <siva0sankar@gmail.com>
2017-03-09 23:14:17 +0530

    before ejs

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,22 @@
 //require express
 var express = require('express');
+var path = require('path');
+
 var router = express.Router();
 
 // export the router
 module.exports = router;
 
 // route the app for homepage
-router.get('/',function(req,res){
-    res.send('Hello World')
+router.get('/', function (req, res) {
+    // res.send('Hello World !!!')
+     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // route for about page
-router.get('/about',function(req,res){
-    res.send('about page ..:)');
+router.get('/about', function (req, res) {
+    // res.send('about page ..:)');
+    res.sendFile(path.join(__dirname, '../about.html'));
 });
 
 router.get('/contact');
--- server.js
+++ server.js
@@ -33,6 +33,10 @@ var port = 8080;
 var router = require('./app/routes');
 app.use('/',router);
 
+// set static file location (css,images and etc)
+app.use(express.static(__dirname + '/public'));
+
+
 // start the server
 app.listen(port,function(){
     console.log('app started');
